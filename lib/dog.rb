7d99ecf3209f1257c0ba9b493a57f4dc9f40fdb0
Edgar V <evenegas9@ucmerced.edu>
2019-09-02 03:24:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,17 @@ class Dog
   def name=(name)
     @name = name
   end
+
+  def breed=(name)
+    @breed = name
+  end
+
   #getter method
   def name
     @name
+  end
+
+  def breed
+    @breed
   end
 end
